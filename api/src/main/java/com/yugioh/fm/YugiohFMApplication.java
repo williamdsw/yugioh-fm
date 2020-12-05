4ea9bdd57e2af3df1c5f467f581c42c51089c0b0
William Santos <williamdsw@outlook.com>
2020-12-05 11:59:52 -0300

    Updating project's name

--- api/src/main/java/com/yugioh/fm/YugiohFMApplication.java
+++ api/src/main/java/com/yugioh/fm/YugiohFMApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YugiohForbiddenMemoriesBackendApiApplication {
+public class YugiohFMApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YugiohForbiddenMemoriesBackendApiApplication.class, args);
+		SpringApplication.run(YugiohFMApplication.class, args);
 	}
 
 }
--- api/src/test/java/com/yugioh/fm/YugiohFMApplicationTests.java
+++ api/src/test/java/com/yugioh/fm/YugiohFMApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class YugiohForbiddenMemoriesBackendApiApplicationTests {
+class YugiohFMApplicationTests {
 
 	@Test
 	void contextLoads() {
